6fc6169cae0d67cd87fb73c8f7d73ba2a8083a97
Overlander <anuragnandal@hotmail.com>
2023-03-16 15:42:12 -0400

    Add new solution

--- easy/binary_search.rb
+++ easy/binary_search.rb
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/binary-search/
 
+# old
+
 def search(nums, target)
   l = 0
   r = nums.length - 1
@@ -15,5 +17,27 @@ def search(nums, target)
       return m
     end
   end
+  return -1
+end
+
+# new (while reading about binary search in A Common-Sense Guide to Data Structures and Algorithms)
+
+def search(nums, target)
+  l = 0
+  r = nums.length - 1
+
+  while l <= r
+    mid = (l + r) / 2
+    mid_val = nums[mid]
+
+    if mid_val == target
+      return mid
+    elsif mid_val > target
+      r = mid - 1
+    elsif mid_val < target
+      l = mid + 1
+    end
+  end
+
   return -1
 end
